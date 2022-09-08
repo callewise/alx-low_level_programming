952ee7beeff22e1211dca0b2f9298e18d06017d2
callewise <mudemifaustin@gmail.com>
2022-09-08 02:18:29 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * *main - Entry point
+ * *
+ * *
+ * *Return: Always 0 (Success)
+ * */
+int main(void)
+{
+
+		char displayed[52];
+
+			strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+				puts(displayed);
+					return (0);
+}
