f7b1c0c23b0262878404819169878c2882fd05f6
callewise <mudemifaustin@gmail.com>
2022-09-13 12:47:41 -0700

    Multiple mainsss

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * main - test functions that prints if integer is
- * positive or negative
+ * main - tests functions that prints if integer is positive or negative
  * Return: 0
  */
 
