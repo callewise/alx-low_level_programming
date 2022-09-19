23693440a94e643d09dd64c6a1bacb347374e85a
callewise <mudemifaustin@gmail.com>
2022-09-19 05:53:20 -0700

    ...defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - returns string length
+ * @c: string to be operated on
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int count, inc;
+	inc = 0;
+	for (count = 0; s[count] != '\0'; count++)
+		inc++;
+
+	return (inc);
+}
