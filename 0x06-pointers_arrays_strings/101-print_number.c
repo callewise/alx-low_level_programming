b1c218baf68f45edc244e625385f2ffc9abe3250
callewise <mudemifaustin@gmail.com>
2022-09-22 12:03:40 -0700

    ...not just symbols on paper

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -4,23 +4,31 @@
  * print_number - print integers
  * @n: integer
  *
- * Return: 0
+ * Return: void
  */
 void print_number(int n)
 {
-	unsigned int n1;
-
-	n1 = n;
+	unsigned int m, k, count;
 
 	if (n < 0)
 	{
-		_putchar('-');
-		n1 = -n;
+		_putchar(45);
+		m = n * -1;
 	}
-
-	if (n1 / 10 != 0)
+	else
 	{
-		print_number(n1 / 10);
+		m = n;
 	}
-	_putchar((n1 % 10) + '0');
+	k = m;
+	count = 1;
+
+	while (k > 9)
+	{
+		k /= 10;
+		count *= 10;
+	}
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
