dbf7f12ea456f0a4e83161ee50459e685e9f4477
callewise <mudemifaustin@gmail.com>
2022-11-01 00:49:42 +0300

    Competed tasks

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,13 +1,9 @@
-#ifndef MAIN_H
-#define MAIN_H
-
-#include <stdlib.h>
-#include <stddef.h>
+#ifndef __MAIN_H__
+#define __MAIN_H__
 
 unsigned int binary_to_uint(const char *b);
-int check_valid_string(const char *b);
-int _putchar(char c);
 void print_binary(unsigned long int n);
+int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
